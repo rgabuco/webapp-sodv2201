80901b972e7a76eb9a4458146896a3eb5e551f17
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-09-29 06:42:43 -0600

    add signup page starter

---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+function Signup() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Signup Page</h1>
+      {/* Add your about content here */}
+    </div>
+  );
+}
+
+export default Signup;
