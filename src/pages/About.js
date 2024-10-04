fb9272be4be7a2176b07f219f39409882ccb0d98
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-10-04 12:51:33 -0600

    correct typo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/navbar/Navbar'; 
-import LoginButton from '../components/login-button/LoginButton'; 
-import ProfileMenu from '../components/profile-menu/ProfileMenu'; 
-import { Container, Box, Typography } from '@mui/material';
-import AboutSection from '../components/about-section/AboutSection';
-import TeamSection from '../components/team-section/Team-Section'; 
-import normalImage from '../resources/img.png';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/navbar/Navbar";
+import LoginButton from "../components/login-button/LoginButton";
+import ProfileMenu from "../components/profile-menu/ProfileMenu";
+import { Container, Box, Typography } from "@mui/material";
+import AboutSection from "../components/about-section/AboutSection";
+import TeamSection from "../components/team-section/TeamSection";
+import normalImage from "../resources/img.png";
 
 function About() {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
@@ -34,21 +34,16 @@ function About() {
   return (
     <div>
       <Navbar rightMenu={userLoggedIn ? <ProfileMenu /> : <LoginButton />} />
-      
+
       <Container sx={{ padding: 0, display: "flex", flexDirection: "column", minHeight: "15vh" }}>
         <AboutSection />
       </Container>
 
       <Container sx={{ marginTop: "35vh", padding: 0, display: "flex", flexDirection: "column" }}>
-        <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 2, color: "#34405E" }}>
+        <Box sx={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", padding: 2, color: "#34405E" }}>
           <Typography variant="h4">The Team</Typography>
         </Box>
-        <TeamSection 
-          members={teamMembers} 
-          hovered={hovered} 
-          handleMouseEnter={handleMouseEnter} 
-          handleMouseLeave={handleMouseLeave} 
-        />
+        <TeamSection members={teamMembers} hovered={hovered} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
       </Container>
     </div>
   );
