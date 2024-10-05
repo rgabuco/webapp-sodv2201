d59711ec8da85cdf844b613ab35bdc3adfec0cdb
enicolecuna <enicole.cuna@gmail.com>
2024-10-05 16:04:13 -0600

    Updated program data

--- src/utils/data/Programs.js
+++ src/utils/data/Programs.js
@@ -2,45 +2,38 @@ import React from "react";
 // Initial dummy data for programs
 const programsArray = [
   {
-    code: "ABCD-2024",
-    name: "Software Development - Diploma",
-    description: "Comprehensive two-year software development diploma program.",
+    code: "SDDBVC-2024",
+    name: "Software Development Diploma",
+    description: "A comprehensive two-year diploma program that equips learners with essential software development skills, focusing on web, mobile, and database applications.",
     term: "Winter",
-    startDate: "January 10, 2024",
+    startDate: "January 10, 2025",
     endDate: "May 15, 2026",
-    fees: "$9,254 domestic / $27,735 international",
-    category: "Technology",
+    fees: "$16,219 domestic / $39,507 international",
+    category: "Software Development Programs",
   },
   {
-    code: "BIZ-MGMT-2024",
-    name: "Business Management - Diploma",
-    description: "A two-year program to develop core business management skills.",
-    term: "Fall",
-    startDate: "September 5, 2024",
-    endDate: "June 15, 2026",
-    fees: "$8,500 domestic / $25,000 international",
-    category: "Business",
-  },
-  {
-    code: "HLTH-NUR-2024",
-    name: "Nursing - Certificate",
-    description: "A one-year certificate program in nursing fundamentals.",
-    term: "Spring",
-    startDate: "March 5, 2024",
-    endDate: "March 5, 2025",
-    fees: "$6,000 domestic / $18,000 international",
-    category: "Health",
-  },
-  {
-    code: "ABCD-2024",
-    name: "Information Technology - Diploma",
-    description: "A comprehensive two-year software development diploma program.",
+    code: "SDPBVC-2024",
+    name: "Software Development Post-Diploma",
+    description: "A comprehensive one-year post-diploma program that equips learners with essential software development skills, focusing on web, mobile, and database applications.",
     term: "Winter",
-    startDate: "January 10, 2024",
-    endDate: "May 15, 2026",
-    fees: "$7,254 domestic / $20,735 international",
-    category: "Technology",
+    startDate: "January 10, 2025",
+    endDate: "Dec 30, 2025",
+    fees: "$8,254 domestic / $21,512 international",
+    category: "Software Development Programs",
+    
   },
+  {
+    code: "SDABVC-2024",
+    name: "Accelerated Software Development Certificate",
+    description: "A comprehensive six-month program that equips learners with essential software development skills, focusing on web, mobile, and database applications",
+    term: "Winter",
+    startDate: "January 10, 2025",
+    endDate: "Oct 30, 2025",
+    fees: "$17,324 domestic / $20,735 international",
+    category: "Software Development Programs",
+    
+  },
+  
 ];
 
 export default programsArray;
