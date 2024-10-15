5ad7694e2cb671757a913a55ab372bef3225216d
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-10-14 18:39:37 -0600

    change dates on dummy data

--- src/utils/data/Messages.js
+++ src/utils/data/Messages.js
@@ -4,7 +4,7 @@ const messagesArray = [
     username: "jdoe",
     email: "john.doe@example.com",
     message: "Hi, I am interested in the course CS101. Can you please provide more details?",
-    date: "2022-01-01T12:00:00.000Z",
+    date: "2024-10-13T12:00:00.000Z",
     isRead: true,
   },
   {
@@ -12,7 +12,7 @@ const messagesArray = [
     username: "asmith",
     email: "allan.smith@example.com",
     message: "I am having trouble logging in. Can you please help?",
-    date: "2022-01-02T12:00:00.000Z",
+    date: "2024-10-14T12:00:00.000Z",
     isRead: false,
   },
   {
@@ -20,7 +20,7 @@ const messagesArray = [
     username: "jdoe",
     email: "john.doe@example.com",
     message: "I have completed the course CS101. Can you please provide the certificate?",
-    date: "2022-01-03T12:00:00.000Z",
+    date: "2022-10-14T12:00:00.000Z",
     isRead: false,
   },
 ];
