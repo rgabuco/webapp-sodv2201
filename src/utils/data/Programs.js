acf89748892b8404c8436842e41ce8ebbe78bb70
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-10-14 20:33:46 -0600

    longer program description

--- src/utils/data/Programs.js
+++ src/utils/data/Programs.js
@@ -3,7 +3,8 @@ const programsArray = [
   {
     code: "SDDBVC-2025",
     name: "Software Development - Diploma",
-    description: "A comprehensive two-year diploma program that equips learners with essential software development skills, focusing on web, mobile, and database applications.",
+    description:
+      "A comprehensive two-year diploma program that equips learners with essential software development skills. This program focuses on web, mobile, and database applications, providing students with hands-on experience and theoretical knowledge. Graduates will be prepared for various roles in the software development industry, including junior developer positions. The curriculum includes project-based learning and industry-relevant technologies to ensure students are job-ready.",
     term: "Winter",
     startDate: "January 10, 2025",
     endDate: "May 15, 2026",
@@ -13,7 +14,8 @@ const programsArray = [
   {
     code: "SDPBVC-2025",
     name: "Software Development - Post Diploma",
-    description: "A comprehensive one-year post-diploma program that equips learners with essential software development skills, focusing on web, mobile, and database applications.",
+    description:
+      "A comprehensive one-year post-diploma program that equips learners with advanced software development skills. This program focuses on advanced topics in web, mobile, and database applications, building on prior knowledge. It is designed for individuals who already have a diploma or degree and are looking to specialize further in software development. The program includes hands-on projects and collaboration with industry partners.",
     term: "Winter",
     startDate: "January 10, 2025",
     endDate: "Dec 30, 2025",
@@ -23,7 +25,8 @@ const programsArray = [
   {
     code: "SDABVC-2025",
     name: "Accelerated Software Development Certificate",
-    description: "A comprehensive six-month program that equips learners with essential software development skills, focusing on web, mobile, and database applications",
+    description:
+      "A comprehensive six-month program that equips learners with essential software development skills. This accelerated program focuses on web, mobile, and database applications, providing intensive training in a short period. It is ideal for individuals looking to quickly gain the skills needed for entry-level positions in the software development industry. The curriculum is designed to be fast-paced and immersive, ensuring rapid skill acquisition.",
     term: "Winter",
     startDate: "January 10, 2025",
     endDate: "Oct 30, 2025",
